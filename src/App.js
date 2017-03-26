ae24d85ae8d03d01ff5e1764716d955104496818
Kamil Koterba <kkoterba@egnyte.com>
2017-03-26 11:03:26 +0200

    Handle field input

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import React from "react";
 import Editor from "./components/Editor";
 import Preview from "./components/Preview";
 
-const App = () => (
-    <div className="container-m">
-        <h2>Markdown Editor</h2>
-        <Editor />
-        <h2>Preview</h2>
-        <Preview />
-    </div>
-);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { text: "" };
+    }
+
+    onTextChange(text) {
+        this.setState({ text });
+    }
+
+    render() {
+        return (
+            <div className="container-m">
+                <h2>Markdown Editor</h2>
+                <Editor onTextChange={ (text) => this.onTextChange(text) } />
+                <h2>Preview</h2>
+                <Preview text={ this.state.text } />
+            </div>
+        )
+    }
+}
+;
 
 export default App;
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-const Editor = () => <input />;
+class Editor extends React.Component {
+    render() {
+        const { onTextChange } = this.props;
+
+        return (
+            <input onChange={ (e) => onTextChange(e.target.value) } />
+        )
+    }
+}
 
 export default Editor;
