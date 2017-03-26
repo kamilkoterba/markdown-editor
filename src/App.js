3479dc65397f1a9f6717c616e731ba8df304b402
Kamil Koterba <kkoterba@egnyte.com>
2017-03-26 10:51:04 +0200

    Editor and Preview boilereplate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 
+import Editor from "./components/Editor";
+import Preview from "./components/Preview";
+
 const App = () => (
     <div className="container-m">
-        <h1>Markdown Editor</h1>
+        <h2>Markdown Editor</h2>
+        <Editor />
+        <h2>Preview</h2>
+        <Preview />
     </div>
 );
 
